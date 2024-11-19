c7e47868b0b4074b49aebba20db2ac5ffbe9d578
sandeep <nanisandeep876@gmail.com>
2024-11-19 18:39:15 +0530

    basic-sql

---
+++ sql-intermediate-skill-certification/Products Without Sales v1.0.sql
@@ -0,0 +1,9 @@
+---- Products Without Sales v1.0
+
+
+SELECT 
+     product.sku, product.product_name
+FROM 
+     product
+WHERE
+     product.id NOT IN (SELECT product_id FROM invoice_item);
---
+++ sql-intermediate-skill-certification/Products Without Sales v2.0.sql
@@ -0,0 +1,11 @@
+---- Products Without Sales v2.0
+SELECT 
+    product.sku, product.product_name
+FROM 
+    product
+WHERE 
+    product.id NOT IN (SELECT product_id FROM invoice_item)
+GROUP BY
+    1,2
+ORDER BY
+    product.sku ASC;
